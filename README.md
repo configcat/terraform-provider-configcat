d1345a0cc3dfb9b71b20850772b22949ec8991b1
Lajos <lajos88.szoke@gmail.com>
2020-10-12 22:49:25 +0200

    Cover

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # terraform-provider-configcat
+
+go test -v -cover -coverprofile=coverage.out ./...
+
+go tool cover -html=coverage.out
--- configcat/resource_configcat_setting_test.go
+++ configcat/resource_configcat_setting_test.go
@@ -55,36 +55,31 @@ func TestResourceSettingValid(t *testing.T) {
 	})
 }
 
-func TestResourceSettingValidSettingTypes(t *testing.T) {
-	const settingsResource = `
+func TestResourceSettingBoolean(t *testing.T) {
+	testResourceSettingForSettingType(t, "boolean")
+}
+
+func TestResourceSettingString(t *testing.T) {
+	testResourceSettingForSettingType(t, "string")
+}
+
+func TestResourceSettingInt(t *testing.T) {
+	testResourceSettingForSettingType(t, "int")
+}
+
+func TestResourceSettingDouble(t *testing.T) {
+	testResourceSettingForSettingType(t, "double")
+}
+
+func testResourceSettingForSettingType(t *testing.T, settingType string) {
+	var settingResource = `
 	resource "configcat_setting" "testBoolean" {
 		config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
-		key = "testBoolKey"
-		name = "testBoolName"
-		setting_type = "boolean"
+		key = "test` + settingType + `"
+		name = "test"
+		setting_type = "` + settingType + `"
 	}
-
-	resource "configcat_setting" "testString" {
-		config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
-		key = "testStringKey"
-		name = "testStringName"
-		setting_type = "string"
-	}
-
-	resource "configcat_setting" "testInt" {
-		config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
-		key = "testIntKey"
-		name = "testIntName"
-		setting_type = "int"
-	}
-
-	resource "configcat_setting" "testDouble" {
-		config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
-		key = "testDoubleKey"
-		name = "testDoubleName"
-		setting_type = "double"
-	}
-`
+	`
 	const configID = "08d86d63-2731-4b8b-823a-56ddda9da038"
 
 	resource.Test(t, resource.TestCase{
@@ -92,35 +87,14 @@ func TestResourceSettingValidSettingTypes(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: settingsResource,
+				Config: settingResource,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("configcat_setting.testBoolean", "id"),
 					resource.TestCheckResourceAttr("configcat_setting.testBoolean", CONFIG_ID, configID),
-					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_KEY, "testBoolKey"),
-					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_NAME, "testBoolName"),
+					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_KEY, "test"+settingType),
+					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_NAME, "test"),
 					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_HINT, ""),
-					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_TYPE, "boolean"),
-
-					resource.TestCheckResourceAttrSet("configcat_setting.testString", "id"),
-					resource.TestCheckResourceAttr("configcat_setting.testString", CONFIG_ID, configID),
-					resource.TestCheckResourceAttr("configcat_setting.testString", SETTING_KEY, "testStringKey"),
-					resource.TestCheckResourceAttr("configcat_setting.testString", SETTING_NAME, "testStringName"),
-					resource.TestCheckResourceAttr("configcat_setting.testString", SETTING_HINT, ""),
-					resource.TestCheckResourceAttr("configcat_setting.testString", SETTING_TYPE, "string"),
-
-					resource.TestCheckResourceAttrSet("configcat_setting.testInt", "id"),
-					resource.TestCheckResourceAttr("configcat_setting.testInt", CONFIG_ID, configID),
-					resource.TestCheckResourceAttr("configcat_setting.testInt", SETTING_KEY, "testIntKey"),
-					resource.TestCheckResourceAttr("configcat_setting.testInt", SETTING_NAME, "testIntName"),
-					resource.TestCheckResourceAttr("configcat_setting.testInt", SETTING_HINT, ""),
-					resource.TestCheckResourceAttr("configcat_setting.testInt", SETTING_TYPE, "int"),
-
-					resource.TestCheckResourceAttrSet("configcat_setting.testDouble", "id"),
-					resource.TestCheckResourceAttr("configcat_setting.testDouble", CONFIG_ID, configID),
-					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_KEY, "testDoubleKey"),
-					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_NAME, "testDoubleName"),
-					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_HINT, ""),
-					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_TYPE, "double"),
+					resource.TestCheckResourceAttr("configcat_setting.testBoolean", SETTING_TYPE, settingType),
 				),
 			},
 		},
