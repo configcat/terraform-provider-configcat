5ff1f2899e557e8bff692d55f31eb8e66c02f3e9
Lajos <lajos88.szoke@gmail.com>
2020-10-12 22:28:27 +0200

    tests

--- configcat/resource_configcat_setting_test.go
+++ configcat/resource_configcat_setting_test.go
@@ -1,6 +1,7 @@
 package configcat
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -76,7 +77,7 @@ func TestResourceSettingValidSettingTypes(t *testing.T) {
 		name = "testIntName"
 		setting_type = "int"
 	}
-	
+
 	resource "configcat_setting" "testDouble" {
 		config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
 		key = "testDoubleKey"
@@ -121,6 +122,29 @@ func TestResourceSettingValidSettingTypes(t *testing.T) {
 					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_HINT, ""),
 					resource.TestCheckResourceAttr("configcat_setting.testDouble", SETTING_TYPE, "double"),
 				),
+			},
+		},
+	})
+}
+
+func TestResourceSettingInvalidSettingType(t *testing.T) {
+	const settingResource = `
+		resource "configcat_setting" "test" {
+			config_id = "08d86d63-2731-4b8b-823a-56ddda9da038"
+			key = "testKey2"
+			name = "testName"
+			setting_type = "invalid"
+		}
+	`
+	const configID = "08d86d63-2731-4b8b-823a-56ddda9da038"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config:      settingResource,
+				ExpectError: regexp.MustCompile(`setting_type parse failed: invalid. Valid values: boolean/string/int/double`),
 			},
 		},
 	})
