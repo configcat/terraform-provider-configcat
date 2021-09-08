3fddbfa85b267a3183fa082c63f2557cd5dc172c
Lajos <lali@configcat.com>
2021-09-08 12:42:41 +0200

    Updating dependencies

--- vendor/github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource/testing.go
+++ vendor/github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource/testing.go
@@ -11,12 +11,14 @@ import (
 	"strings"
 
 	"github.com/hashicorp/go-multierror"
-	tftest "github.com/hashicorp/terraform-plugin-test/v2"
 	testing "github.com/mitchellh/go-testing-interface"
 
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/internal/addrs"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/internal/plugintest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
@@ -257,6 +259,9 @@ type ImportStateCheckFunc func([]*terraform.InstanceState) error
 // generation for ImportState tests.
 type ImportStateIdFunc func(*terraform.State) (string, error)
 
+// ErrorCheckFunc is a function providers can use to handle errors.
+type ErrorCheckFunc func(error) error
+
 // TestCase is a single acceptance test case used to test the apply/destroy
 // lifecycle of a resource in a specific configuration.
 //
@@ -296,6 +301,18 @@ type TestCase struct {
 	//  }
 	ProviderFactories map[string]func() (*schema.Provider, error)
 
+	// ProtoV5ProviderFactories serves the same purpose as ProviderFactories,
+	// but for protocol v5 providers defined using the terraform-plugin-go
+	// ProviderServer interface.
+	ProtoV5ProviderFactories map[string]func() (tfprotov5.ProviderServer, error)
+
+	// ProtoV6ProviderFactories serves the same purpose as ProviderFactories,
+	// but for protocol v6 providers defined using the terraform-plugin-go
+	// ProviderServer interface.
+	// The version of Terraform used in acceptance testing must be greater
+	// than or equal to v0.15.4 to use ProtoV6ProviderFactories.
+	ProtoV6ProviderFactories map[string]func() (tfprotov6.ProviderServer, error)
+
 	// Providers is the ResourceProvider that will be under test.
 	//
 	// Deprecated: Providers is deprecated, please use ProviderFactories
@@ -316,6 +333,10 @@ type TestCase struct {
 	// CheckDestroy is called after the resource is finally destroyed
 	// to allow the tester to test that the resource is truly gone.
 	CheckDestroy TestCheckFunc
+
+	// ErrorCheck allows providers the option to handle errors such as skipping
+	// tests based on certain errors.
+	ErrorCheck ErrorCheckFunc
 
 	// Steps are the apply sequences done within the context of the
 	// same state. Each step can have its own check to verify correctness.
@@ -534,8 +555,8 @@ func Test(t testing.T, c TestCase) {
 	if err != nil {
 		t.Fatalf("Error getting working dir: %s", err)
 	}
-	helper := tftest.AutoInitProviderHelper(sourceDir)
-	defer func(helper *tftest.Helper) {
+	helper := plugintest.AutoInitProviderHelper(sourceDir)
+	defer func(helper *plugintest.Helper) {
 		err := helper.Close()
 		if err != nil {
 			log.Printf("Error cleaning up temporary test files: %s", err)
