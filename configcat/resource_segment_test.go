3f42eba2083a10ff838a2ace03059e29e2284846
Lali <lali@configcat.com>
2022-02-02 19:04:42 +0100

    test fix

--- configcat/resource_segment_test.go
+++ configcat/resource_segment_test.go
@@ -30,7 +30,7 @@ func TestResourceSegmentFlow(t *testing.T) {
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_NAME, "testName"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_DESCRIPTION, "testDescription"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_ATTRIBUTE, "email"),
-					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARATOR, "isOneOf"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARATOR, "sensitiveIsOneOf"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_VALUE, "a@b.com,c@d.com"),
 				),
 			},
@@ -43,7 +43,7 @@ func TestResourceSegmentFlow(t *testing.T) {
 						name = "testName2"
 						description = "testDescription2"
 						comparison_attribute = "version"
-						comparator = "semverLess"
+						comparator = "semVerLess"
 						comparison_value="2.0.0"
 					}
 				`,
@@ -52,7 +52,7 @@ func TestResourceSegmentFlow(t *testing.T) {
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_NAME, "testName2"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_DESCRIPTION, "testDescription2"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_ATTRIBUTE, "version"),
-					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARATOR, "semverLess"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARATOR, "semVerLess"),
 					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_VALUE, "2.0.0"),
 				),
 			},
@@ -84,6 +84,28 @@ func TestResourceSegmentWrongComparator(t *testing.T) {
 				}
 				`,
 				ExpectError: regexp.MustCompile(`could not parse Comparator`),
+			},
+			{
+				Config: `
+				data "configcat_products" "products" {
+				}
+				resource "configcat_segment" "test" {
+					product_id = data.configcat_products.products.products.0.product_id
+					name = "testName"
+					description = "testDescription"
+					comparison_attribute = "email"
+					comparator = "sensitiveIsOneOf"
+					comparison_value="a@b.com,c@d.com"
+				}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("configcat_segment.test", "id"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_NAME, "testName"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_DESCRIPTION, "testDescription"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_ATTRIBUTE, "email"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARATOR, "sensitiveIsOneOf"),
+					resource.TestCheckResourceAttr("configcat_segment.test", SEGMENT_COMPARISON_VALUE, "a@b.com,c@d.com"),
+				),
 			},
 			{
 				ResourceName:      "configcat_segment.test",
