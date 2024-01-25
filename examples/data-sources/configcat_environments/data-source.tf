e6134cc0f07e0b445e1a9f18bf77a244929b5b0c
Lajos Szoke <lali@configcat.com>
2024-01-25 12:51:59 +0100

    docs

---
+++ examples/data-sources/configcat_environments/data-source.tf
@@ -0,0 +1,13 @@
+variable "product_id" {
+  type = string
+}
+
+data "configcat_environments" "my_environments" {
+  product_id        = var.product_id
+  name_filter_regex = "Test"
+}
+
+
+output "environment_id" {
+  value = data.configcat_environments.my_environments.environments.0.environment_id
+}
