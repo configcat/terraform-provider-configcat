7a86a915eea6e8800eba33747603d0b5525c3b43
Lajos Szoke <lali@configcat.com>
2024-01-26 04:02:17 +0100

    perm group resource

--- internal/configcat/provider.go
+++ internal/configcat/provider.go
@@ -177,6 +177,7 @@ func (p *configCatProvider) Resources(ctx context.Context) []func() resource.Res
 		NewConfigResource,
 		NewEnvironmentResource,
 		NewSettingResource,
+		NewPermissionGroupResource,
 		NewSegmentResource,
 		NewTagResource,
 		NewSettingTagResource,
