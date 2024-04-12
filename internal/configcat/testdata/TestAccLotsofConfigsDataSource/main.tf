7b5b887f81409be35fdcbfcf2d13bb3b792f8e09
Lajos Szoke <lali@configcat.com>
2024-04-12 21:15:24 +0200

    lint fix

--- internal/configcat/testdata/TestAccLotsofConfigsDataSource/main.tf
+++ internal/configcat/testdata/TestAccLotsofConfigsDataSource/main.tf
@@ -17,3 +17,7 @@ data "configcat_configs" "test" {
   product_id        = var.product_id
   name_filter_regex = var.name_filter_regex
 }
+
+output "config_id" {
+  value = length(data.configcat_configs.test[0].configs) > 0 ? data.configcat_configs.test[0].configs[0].config_id : null
+}
