7abfc0a882e1e23259038ed24394c3d3653a2b1e
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2024-08-09 11:51:37 +0200

    Setting tag remove fix (#55)
    * Setting tag remove fix
    
    * multiple tag tests

---
+++ internal/configcat/testdata/TestAccSettingTagMultipleResource/removeonetag.tf
@@ -0,0 +1,40 @@
+variable "product_id" {
+  type = string
+}
+
+variable "config_id" {
+  type = string
+}
+
+resource "configcat_setting" "testSetting" {
+  config_id = var.config_id
+  key       = "testkeywithtag"
+  name      = "test"
+  order     = 0
+}
+
+resource "configcat_tag" "testTag1" {
+  product_id = var.product_id
+  name       = "tag1"
+}
+
+resource "configcat_tag" "testTag2" {
+  product_id = var.product_id
+  name       = "tag2"
+}
+
+resource "configcat_tag" "testTag3" {
+  product_id = var.product_id
+  name       = "tag3"
+}
+
+resource "configcat_setting_tag" "settingTag1" {
+  setting_id = configcat_setting.testSetting.id
+  tag_id     = configcat_tag.testTag1.id
+}
+
+resource "configcat_setting_tag" "settingTag3" {
+  setting_id = configcat_setting.testSetting.id
+  tag_id     = configcat_tag.testTag3.id
+}
+
