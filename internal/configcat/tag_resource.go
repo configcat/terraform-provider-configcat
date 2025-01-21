72c5ac5703c576f0fbd0d7284de5e955473e332d
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2025-01-21 14:07:57 +0100

    Revert internal + Export provider (#65)
    * Revert "drop internal package (#64)"
    
    This reverts commit 430a428031e9066fafcfb329959060e460676cee.
    
    * provider link to be able to use from pulumi

--- internal/configcat/tag_resource.go
+++ internal/configcat/tag_resource.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/configcat/terraform-provider-configcat/v5/configcat/client"
+	"github.com/configcat/terraform-provider-configcat/v5/internal/configcat/client"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
