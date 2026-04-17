6eb35d4ce08d0aff0591936a1fab5d3aba4e1538
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2026-04-17 15:38:16 +0200

    permission group test fixes (#90)

--- internal/configcat/permission_groups_data_source_test.go
+++ internal/configcat/permission_groups_data_source_test.go
@@ -76,7 +76,7 @@ func TestAccPermissionGroupsDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr(testResourceName, listAttribute+".0."+PermissionGroupCanDisable2FA, "true"),
 					resource.TestCheckResourceAttr(testResourceName, listAttribute+".0."+PermissionGroupAccessType, "full"),
 					resource.TestCheckResourceAttr(testResourceName, listAttribute+".0."+PermissionGroupNewEnvironmentAccessType, "full"),
-					resource.TestCheckResourceAttr(testResourceName, listAttribute+".0."+PermissionGroupEnvironmentAccess+".%", "3"),
+					resource.TestCheckResourceAttr(testResourceName, listAttribute+".0."+PermissionGroupEnvironmentAccess+".%", "0"),
 				),
 			},
 			{
